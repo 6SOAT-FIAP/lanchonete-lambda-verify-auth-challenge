465b09d752c18085bccca31b7606c1723e2ba5cc
DeividCezar <perspicilum@gmail.com>
2024-09-25 23:26:14 -0300

    Create infra

---
+++ infra/main.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+  region = var.aws_region
+}
+
+resource "aws_lambda_function" "lanchonete_lambda_auth" {
+  function_name    = var.lambda_function_name
+  handler          = var.lambda_handler
+  runtime          = var.lambda_runtime
+  role             = var.lambda_role
+  filename         = var.lambda_zip_path
+  source_code_hash = filebase64sha256(var.lambda_zip_path)
+}
